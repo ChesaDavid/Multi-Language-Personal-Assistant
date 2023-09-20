f0c81c38b002ebe5b4b636f4f1e9f9e05cfb2908
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2023-09-20 23:31:14 +0300

    The Game is the game

---
+++ SpeachRecognicion.py
@@ -0,0 +1,14 @@
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+
+try:
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(command)
+
+except:
+    print('speech not recognized')
+    pass
