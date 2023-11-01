caa015c489819fea27c9cba93275979d07d79bf3
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2023-11-02 00:29:46 +0200

    New try

--- FinalSpeechRecognitionOnlyEnlish.py
+++ FinalSpeechRecognitionOnlyEnlish.py
@@ -10,23 +10,11 @@ with sr.Microphone() as source:
 
 # Utilizarea motorului de recunoaștere vocală Google pentru a converti vorbirea în text
 try:
+
     text = recognizer.recognize_google(audio)
     print("Ați spus: " + text)
+    
 except sr.UnknownValueError:
     print("Nu am putut recunoaște vorbirea.")
 except sr.RequestError as e:
     print("Eroare la cererea de recunoaștere vocală; {0}".format(e))
-
-
-#
-#recorder=sr.AudioFile('Microphone.wav')
-#try:
-#    with Microphone() as source:
-#        print('listening...')
-#        audio = recorder.record(source,duration=duration1)
-#        command = listener.recognize_google(audio)
-#        print(command)
-#   
-#except:
-#    print('speech not recognized')
-#    pass    
