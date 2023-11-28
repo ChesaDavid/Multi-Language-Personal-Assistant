3aa5b01c44ccce8f60b31556846f2ba9a60d98e6
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2023-11-28 22:46:12 +0200

    Flame for singup page and java task management

--- .allforjava/TaskManagement.java
+++ .allforjava/TaskManagement.java
@@ -1,161 +1,163 @@
-package com.nice.taskmanager;
-
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-
-
-public class TaskManager 
-{
-	private static final int DEFAULT_MAX_CAPACITY = 30;
-
-	public static final int PRIORITY_HIGH = 2;
-	public static final int PRIORITY_LOW = 0;
-
-	private int maxCapacity = DEFAULT_MAX_CAPACITY;
-	
-	
-	private BlockingQueue<Task> highPriorityQ = new LinkedBlockingQueue<Task>();
-	private BlockingQueue<Task> normalPriorityQ = new LinkedBlockingQueue<Task>();
-	private BlockingQueue<Task> lowPriorityQ = new LinkedBlockingQueue<Task>();
-	
-	private List< BlockingQueue<Task> > vector = new ArrayList< BlockingQueue<Task> >();
-
-	//"waiting list":
-	//keep them sorted by the timeToInsert, so if we have many items we do not have to iterate all:
-	private SortedSet<TimedTask> reoccurences = new TreeSet<TimedTask>();
-
-	
-	public TaskManager()
-	{
-		vector.add(lowPriorityQ);
-		vector.add(normalPriorityQ);
-		vector.add(highPriorityQ);
-	}
-	
-	/**
-	 * 
-	 * @param jobNumber
-	 * @param priority
-	 * @return false if there is no space for this new task. true if successful
-	 * @throws IOException 
-	 */
-			
-	public boolean insertTask(int jobNumber, int priority) throws IOException
-	{
-		return insertTask(jobNumber, priority, 0);		
-	}
-	
-	/**
-	 * 
-	 * @param jobNumber
-	 * @param priority
-	 * @param interval
-	 * @return false if there is no space for this new task. true if successful
-	 * @throws IOException 
-	 */
-	public boolean insertTask(int jobNumber, int priority, int interval) throws IOException
-	{
-		handleWaitingReoccurences();
-
-		return innerInsertTask(jobNumber, priority, interval);				
-	}
-
-	
-	private boolean innerInsertTask(int jobNumber, int priority, int interval) throws IOException
-	{
-		//validate input....
-		if(priority < PRIORITY_LOW || priority > PRIORITY_HIGH)
-		{
-			//error
-			throw new IOException("Error: bad priority value");
-		}
-
-		//first of all, make sure we have room for this new task
-		int numElements = 0;
-		for( BlockingQueue<Task> q : vector )
-		{
-			numElements += q.size();
-		}
-		if(numElements >= maxCapacity)
-		{
-			return false;
-		}
-
-		Task taskToInsert = new Task(jobNumber, priority, interval);
-				
-		//check the priority to analyse what Q to insert to:
-		BlockingQueue<Task> q = vector.get( priority );
-		
-		//if Q is full, offer() returns false. true upon success:
-		return q.offer(taskToInsert);
-				
-	}
-
-	
-	private void handleWaitingReoccurences() throws IOException
-	{
-		for(TimedTask timedTask : reoccurences)
-		{
-			if(timedTask.timeHasPassed())
-			{
-				innerInsertTask(timedTask.jobNumber, timedTask.priority, timedTask.interval);
-			}
-			else		//we meet item that its time hasn;t arrived - so we can stop iteration
-				//this structure is sorted by the time-to-insert
-			{
-				return;
-			}
-		}
-	}
-
-	/**
-	 * gets the highest priority task. seach in all Qs, by priority
-	 * @return -1 if no jobs are available
-	 * @throws IOException 
-	 */
-	public int getNextJob() throws IOException
-	{
-		handleWaitingReoccurences();
-
-		//NOTE: reverse iteration 
-		for(ListIterator< BlockingQueue<Task> > it = vector.listIterator(vector.size()); it.hasPrevious(); )
-		//for(BlockingQueue<Task> q : vector)
-		{
-			BlockingQueue<Task> q = it.previous();
-			Task task = q.poll(); 		//TODO: check poll()
-			if( task == null)
-				continue;		//to the next Q
-			else
-			{
-				//if this is reoccurenceTask, handle it:
-				if(task.getInterval() != 0)
-				{
-					//put it in a waiting list:
-					TimedTask timedTask = new TimedTask(task);
-					reoccurences.add(timedTask);
-				}
-				return task.getJobNumber();
-			}
-		}
-				
-		return -1;		//did not find any task in all Qs
-	}
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import java.lang.*;
+public class TaskKiller {
+@SuppressWarnings("IndexOfReplaceableByContains")
+public static void main(String[] args) {
+    JOptionPane pane = new JOptionPane();
+    String userInput1 = JOptionPane.showInputDialog("Hours: "); 
+    String userInput2 = JOptionPane.showInputDialog("Minutes: ");
+    String userInput3 = JOptionPane.showInputDialog("Seconds: ");
+    long inputHours = Long.parseLong(userInput1);
+    long inputMinutes = Long.parseLong(userInput2);
+    long inputSeconds = Long.parseLong(userInput3);
+    long hours = inputHours * 3600000;
+    long minutes = inputMinutes * 60000;
+    long seconds = inputSeconds * 1000;
+    long time = (hours + minutes) + seconds;
+    String OS = System.getProperty("os.name").toLowerCase();
+ int run = 1;   
 
 
 
-	public int getMaxCapacity() {
-		return maxCapacity;
-	}
+while (run > 0){
+ JOptionPane.showMessageDialog(null,"Program names: chrome, firefox, safari, gta, csgo", "Info", JOptionPane.INFORMATION_MESSAGE);
+    String prog = JOptionPane.showInputDialog("Program: ");
+    String gta="gta";
+    String chrome="chrome";
+    String firefox="firefox";     
+    String safari="safari";   
+    String csgo="csgo";       
+    int gt=prog.compareTo(gta);
+    int ch=prog.compareTo(chrome);
+    int fi=prog.compareTo(firefox);
+    int sa=prog.compareTo(safari);
+    int cs=prog.compareTo(csgo);
 
-	public void setMaxCapacity(int maxCapacity) 
-	{
-		this.maxCapacity = maxCapacity;
-	}
+if (gt == 0) {
+    if(OS.indexOf("win") >= 0){
+    try {
+        TimeUnit.MILLISECONDS.sleep(time);
+    Runtime.getRuntime().exec("cmd /c Taskkill /IM GTA5.exe /F");
+        TimeUnit.MILLISECONDS.sleep(5000);
+        Runtime.getRuntime().exec("cmd /c Taskkill /IM GTAVLauncher.exe /F");
+        System.out.println("Win");
+        run=run-1;
+    }
+    catch (InterruptedException ex) {
+    } 
+    catch (IOException ex) {
+        Logger.getLogger(TaskKiller.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }else if(OS.indexOf("mac") >= 0){
+        try {
+            Runtime.getRuntime().exec("/bin/bash -c  sudo killall process PID");
+            System.out.println("Mac");
+            run=run-1;
+        } catch (IOException ex) {
+            Logger.getLogger(TaskKiller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+else if (ch==0) {
+if(OS.indexOf("win") >= 0){
+    try {
+        TimeUnit.MILLISECONDS.sleep(time);
+    Runtime.getRuntime().exec("cmd /c Taskkill /IM chrome.exe /F");
+        System.out.println("Win");
+        run=run-1;
+    }
+    catch (InterruptedException ex) {
+    } 
+    catch (IOException ex) {
+        Logger.getLogger(TaskKiller.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }else if(OS.indexOf("mac") >= 0){
+        try {
+            Runtime.getRuntime().exec("/bin/bash -c  sudo killall process PID");
+            System.out.println("Mac");
+            run=run-1;
+        } catch (IOException ex) {
+            Logger.getLogger(TaskKiller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+else if (fi==0) {
+    if(OS.indexOf("win") >= 0){
+    try {
+        TimeUnit.MILLISECONDS.sleep(time);
+    Runtime.getRuntime().exec("cmd /c Taskkill /IM firefox.exe /F");
+        System.out.println("Win");
+        run=run-1;
+    }
+    catch (InterruptedException ex) {
+    } 
+    catch (IOException ex) {
+        Logger.getLogger(TaskKiller.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }else if(OS.indexOf("mac") >= 0){
+        try {
+            Runtime.getRuntime().exec("/bin/bash -c  sudo killall process PID");
+            System.out.println("Mac");
+            run=run-1;
+        } catch (IOException ex) {
+            Logger.getLogger(TaskKiller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+else if (sa==0) {
+    if(OS.indexOf("win") >= 0){
+    try {
+        TimeUnit.MILLISECONDS.sleep(time);
+    Runtime.getRuntime().exec("cmd /c Taskkill /IM safari.exe /F");
+        System.out.println("Win");
+        run=run-1;
+    }
+    catch (InterruptedException ex) {
+    } 
+    catch (IOException ex) {
+        Logger.getLogger(TaskKiller.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }else if(OS.indexOf("mac") >= 0){
+        try {
+            Runtime.getRuntime().exec("/bin/bash -c  sudo killall process PID");
+            System.out.println("Mac");
+            run=run-1;
+        } catch (IOException ex) {
+            Logger.getLogger(TaskKiller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+else if (cs==0) {
+    if(OS.indexOf("win") >= 0){
+    try {
+        TimeUnit.MILLISECONDS.sleep(time);
+    Runtime.getRuntime().exec("cmd /c Taskkill /IM csgo.exe /F");
+        System.out.println("Win");
+        run=run-1;
+    }
+    catch (InterruptedException ex) {
+    } 
+    catch (IOException ex) {
+        Logger.getLogger(TaskKiller.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }else if(OS.indexOf("mac") >= 0){
+        try {
+            Runtime.getRuntime().exec("/bin/bash -c  sudo killall process PID");
+            System.out.println("Mac");
+            run=run-1;
+        } catch (IOException ex) {
+            Logger.getLogger(TaskKiller.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+else {
+    JOptionPane.showMessageDialog(null, "Invalid name. Try Again or quit", "Error", JOptionPane.ERROR_MESSAGE);
+    run=run+1;
+}
+}
+}
 }
